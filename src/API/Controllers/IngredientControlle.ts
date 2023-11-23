b0600c6339e61b68db8aa4538aa856bf5734c558
Lucas Machado <lukaasmachado143@gmail.com>
2023-11-23 17:53:28 -0300

    fix: reoganizing methods.

--- src/API/Controllers/IngredientControlle.ts
+++ src/API/Controllers/IngredientControlle.ts
@@ -2,45 +2,61 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { GeneralResponse } from "../../CORE/Types/GeneralResponse";
 export class IngredientController {
   async getAll(request: FastifyRequest, reply: FastifyReply) {
+    const response: GeneralResponse = {
+      success: false,
+      message: "",
+    };
     try {
+      response.success = true;
+      response.data.message = "Prosseguir";
+      return response;
     } catch (error) {
-      const response: GeneralResponse = {
-        success: false,
-        message: "Internal Error",
-      };
+      response.message = "Internal Error, try again !";
       return reply.status(500).send(response);
     }
   }
 
   async create(request: FastifyRequest, reply: FastifyReply) {
+    const response: GeneralResponse = {
+      success: false,
+      message: "",
+    };
     try {
+      response.success = true;
+      response.data.message = "Prosseguir";
+      return response;
     } catch (error) {
-      const response: GeneralResponse = {
-        success: false,
-        message: "Internal Error",
-      };
+      response.message = "Internal Error, try again !";
       return reply.status(500).send(response);
     }
   }
 
   async update(request: FastifyRequest, reply: FastifyReply) {
+    const response: GeneralResponse = {
+      success: false,
+      message: "",
+    };
     try {
+      response.success = true;
+      response.data.message = "Prosseguir";
+      return response;
     } catch (error) {
-      const response: GeneralResponse = {
-        success: false,
-        message: "Internal Error",
-      };
+      response.message = "Internal Error, try again !";
       return reply.status(500).send(response);
     }
   }
 
   async delete(request: FastifyRequest, reply: FastifyReply) {
+    const response: GeneralResponse = {
+      success: false,
+      message: "",
+    };
     try {
+      response.success = true;
+      response.data.message = "Prosseguir";
+      return response;
     } catch (error) {
-      const response: GeneralResponse = {
-        success: false,
-        message: "Internal Error",
-      };
+      response.message = "Internal Error, try again !";
       return reply.status(500).send(response);
     }
   }
