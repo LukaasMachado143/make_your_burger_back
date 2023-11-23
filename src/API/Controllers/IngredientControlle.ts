e24bc757b700b3ebf00f6adfe22a70d8d4e8d3b3
Lucas Machado <lukaasmachado143@gmail.com>
2023-11-23 19:37:06 -0300

    feat: implementing all methods.

--- src/API/Controllers/IngredientControlle.ts
+++ src/API/Controllers/IngredientControlle.ts
@@ -1,14 +1,17 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { GeneralResponse } from "../../CORE/Types/GeneralResponse";
+import { IIngredientService } from "../../CORE/Interfaces/IIngredientService";
+import { IngredientService } from "../../SERVICES/IngredientService";
+import { Ingredient } from "@prisma/client";
 export class IngredientController {
   async getAll(request: FastifyRequest, reply: FastifyReply) {
+    const service: IIngredientService = new IngredientService();
     const response: GeneralResponse = {
       success: false,
       message: "",
     };
     try {
-      response.success = true;
-      response.data.message = "Prosseguir";
+      const response: GeneralResponse = await service.getAll();
       return response;
     } catch (error) {
       response.message = "Internal Error, try again !";
@@ -16,46 +19,63 @@ export class IngredientController {
     }
   }
 
-  async create(request: FastifyRequest, reply: FastifyReply) {
+  async create(
+    request: FastifyRequest<{ Body: Ingredient }>,
+    reply: FastifyReply
+  ) {
+    const service: IIngredientService = new IngredientService();
     const response: GeneralResponse = {
       success: false,
       message: "",
     };
     try {
-      response.success = true;
-      response.data.message = "Prosseguir";
+      const newIngredient: Ingredient = request.body;
+      const response = await service.create(newIngredient);
       return response;
     } catch (error) {
+      response.success = false;
+      response.message = "Internal Error, try again !";
+      response.error = error;
+      return reply.status(500).send(response);
+    }
+  }
+
+  async update(
+    request: FastifyRequest<{ Params: { id: number; name: string } }>,
+    reply: FastifyReply
+  ) {
+    const service: IIngredientService = new IngredientService();
+    const response: GeneralResponse = {
+      success: false,
+      message: "",
+    };
+
+    try {
+      const { id, name } = request.params;
+      const response = await service.update(id, name);
+      return response;
+    } catch (error) {
+      response.success = false;
       response.message = "Internal Error, try again !";
       return reply.status(500).send(response);
     }
   }
 
-  async update(request: FastifyRequest, reply: FastifyReply) {
+  async delete(
+    request: FastifyRequest<{ Params: { id: number } }>,
+    reply: FastifyReply
+  ) {
+    const service: IIngredientService = new IngredientService();
     const response: GeneralResponse = {
       success: false,
       message: "",
     };
     try {
-      response.success = true;
-      response.data.message = "Prosseguir";
+      const id: number = request.params.id;
+      const response: GeneralResponse = await service.delete(id);
       return response;
     } catch (error) {
-      response.message = "Internal Error, try again !";
-      return reply.status(500).send(response);
-    }
-  }
-
-  async delete(request: FastifyRequest, reply: FastifyReply) {
-    const response: GeneralResponse = {
-      success: false,
-      message: "",
-    };
-    try {
-      response.success = true;
-      response.data.message = "Prosseguir";
-      return response;
-    } catch (error) {
+      response.success = false;
       response.message = "Internal Error, try again !";
       return reply.status(500).send(response);
     }
