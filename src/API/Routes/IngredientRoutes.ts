3a56960bece05535bb1eafe46f31ea94a111d858
Lucas Machado <lukaasmachado143@gmail.com>
2023-11-23 19:37:35 -0300

    fix: alter mode for picking updated name.

--- src/API/Routes/IngredientRoutes.ts
+++ src/API/Routes/IngredientRoutes.ts
@@ -5,6 +5,6 @@ export const IngredientRoutes = async (fastify: FastifyInstance) => {
   const controller = new IngredientController();
   fastify.get("/", controller.getAll);
   fastify.post("/", controller.create);
-  fastify.put("/:id", controller.update);
+  fastify.put("/:id/:name", controller.update);
   fastify.delete("/:id", controller.delete);
 };
